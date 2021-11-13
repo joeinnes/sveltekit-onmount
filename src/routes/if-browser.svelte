b444f5ea8980693818fdabca8c8f91396c16d50a
Joe Innes <joe@joeinn.es>
2021-11-13 15:29:31 +0100

    Add some timeouts to simulate async data fetching and promises resolving

--- src/routes/if-browser.svelte
+++ src/routes/if-browser.svelte
@@ -4,9 +4,13 @@
 	debugger;
 	if (browser) {
 		colour = 'red';
+		setTimeout(() => {
+			colour = 'green';
+		}, 3000);
 	}
 </script>
 
 <div style="background-color: {colour}">
-	This div should be blue if the component is server rendered or red in the browser.
+	This div should be blue if the component is server rendered or red in the browser, and change to
+	green after 3 seconds.
 </div>
--- src/routes/on-mount.svelte
+++ src/routes/on-mount.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	let colour = 'blue';
 	import { onMount } from 'svelte';
-	onMount(() => (colour = 'red'));
+	onMount(() => {
+		setTimeout(() => {
+			colour = 'green';
+		}, 3000);
+		colour = 'red';
+	});
 </script>
 
 <div style="background-color: {colour}">
-	This div should be blue if the component is server rendered or red in the browser.
+	This div should be blue if the component is server rendered or red in the browser, and change to
+	green after 3 seconds.
 </div>
